ef4c2cc12f014075e0ebdbffc802431b4b01d6d9
FabioNog <fabio.carmo@computacao.ufla.br>
2020-03-15 18:17:12 -0300

    Alter link for my create products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,7 +1,9 @@
 <h3>Welcome Products</h3>
-
+<a href="{{route('products.create')}}">Insert new Products</a>
 <ol>
     @foreach ($products as $p)
-        <li>{{$p['prod_nome']}}</li>
+        <li>
+            {{$p['prod_nome']}};
+        </li>
     @endforeach
 </ol>
