7dac9b2b584d9edba6320d981ec53615edb55cc2
FabioNog <fabio.carmo@computacao.ufla.br>
2020-04-12 12:48:29 -0300

    Add  migrates products and brand

---
+++ database/migrations/2020_04_11_161659_create_products.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtcs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('stock');
+            $table->double('price');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtcs');
+    }
+}
